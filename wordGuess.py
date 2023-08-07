f2d1398cbb38b73dabe9905658cb4c04080e4e6f
Prakhar Pratap <prakharpratap2001@gmail.com>
2023-08-07 12:24:29 +0530

    easy

---
+++ wordGuess.py
@@ -0,0 +1,48 @@
+import random
+
+# library that we use in order to choose
+# on random words from a list of words
+
+name = input("What is your name? : ")  # getting input from the user
+
+print(f"Good Luck ! {name}")
+
+words = ['rainbow', 'computer', 'science', 'programming',
+         'python', 'mathematics', 'player', 'condition',
+         'reverse', 'water', 'board', 'geeks']
+
+word = random.choice(words)  # this function will choose one random word form this list of words
+
+print("Guess the characters")
+
+guesses = ""
+
+turns = 12  # any number of turns can be used here
+
+while turns > 0:
+    failed = 0  # count the number of times a user fails
+    for char in word:  # all characters from the input word taking one at a time
+        if char in guesses:
+            print(char, end=" ")
+        else:
+            print("_", end=" ")
+            failed += 1  # for every failure 1 will be incremented in failure
+
+    if failed == 0:  # user will win the game if failure is 0
+        print("\nYou Win")  # this will be the output
+        print(f"The word is {word}")  # this prints the correct word
+        break
+
+    print()
+    guess = input("Guess a character: ")
+
+    guesses += guess  # storing every input character in guesses
+
+    # check input with the character in the word
+    if guess not in word:
+        turns -= 1
+        print("Wrong")  # if the character doesn't match the word then "Wrong" will be given as output
+        print(f"You have {turns} more guesses")
+
+        if turns == 0:
+            print("You Lose")
